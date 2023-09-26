3fb7d2a739317728db0fd04c455747c718173339
Miguel Ángel <miguel.php@gmail.com>
2023-09-26 01:47:20 -0300

    Update transactionController.php
    Funciones Creadas

--- api/v1/2023/controllers/transactionController.php
+++ api/v1/2023/controllers/transactionController.php
@@ -19,97 +19,346 @@ class TransactionController
     }
 
     // Función para obtener todos los registros de transacciones
-    public function getAllTransactions()
-    {
-        // Implementa la lógica para obtener todos los registros de transacciones
-		echo "imprimir todos los registros";
-    }
+	public function getAllTransactions()
+	{
+		try {
+			// Preparar la consulta SQL
+			$query = "SELECT * FROM transactions";
 
-    // Función para obtener una transacción por su ID
-    public function getTransactionById($transactionId)
-    {
-        // Implementa la lógica para obtener una transacción por su ID
-		echo "imprimir registros por id";
-    }
+			// Preparar la declaración PDO
+			$statement = $this->db->prepare($query);
+
+			// Ejecutar la consulta
+			$statement->execute();
+
+			// Obtener todos los registros como un arreglo asociativo
+			$transactions = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+			// Verificar si se obtuvieron registros
+			if (empty($transactions)) {
+				// Si no se encontraron registros, puedes devolver un mensaje personalizado o un arreglo vacío
+				$response = [
+					"message" => "No se encontraron registros de transacciones."
+				];
+			} else {
+				// Si se encontraron registros, puedes devolverlos en formato JSON
+				$response = $transactions;
+			}
+
+			return json_encode($response, JSON_PRETTY_PRINT);
+		} catch (PDOException $e) {
+			// En caso de error en la consulta, puedes manejarlo aquí
+			return json_encode(["error" => $e->getMessage()], JSON_PRETTY_PRINT);
+		}
+	}
+
+	public function getTransactionById($transactionId)
+	{
+		try {
+			// Preparar la consulta SQL para obtener la transacción por su ID
+			$query = "SELECT * FROM transactions WHERE id = :transaction_id";
+
+			// Preparar la declaración PDO
+			$statement = $this->db->prepare($query);
+
+			// Bind de los parámetros
+			$statement->bindParam(":transaction_id", $transactionId, PDO::PARAM_INT);
+
+			// Ejecutar la consulta
+			$statement->execute();
+
+			// Obtener la transacción como un arreglo asociativo
+			$transaction = $statement->fetch(PDO::FETCH_ASSOC);
+
+			// Verificar si se encontró la transacción
+			if (!$transaction) {
+				// Si no se encontró la transacción, puedes devolver un mensaje personalizado o un arreglo vacío
+				$response = [
+					"message" => "No se encontró la transacción con el ID especificado."
+				];
+			} else {
+				// Si se encontró la transacción, puedes devolverla en formato JSON
+				$response = $transaction;
+			}
+
+			return json_encode($response, JSON_PRETTY_PRINT);
+		} catch (PDOException $e) {
+			// En caso de error en la consulta, puedes manejarlo aquí
+			return json_encode(["error" => $e->getMessage()], JSON_PRETTY_PRINT);
+		}
+	}
 
     // Función para buscar transacciones por descripción
-    public function getTransactionsByDescription($description)
-    {
-        // Implementa la lógica para buscar transacciones por descripción
-		echo "Imprimir registros por descripcion";
-    }
+	public function getTransactionsByDescription($description)
+	{
+		try {
+			// Preparar la consulta SQL para buscar transacciones por descripción
+			$query = "SELECT * FROM transactions WHERE description LIKE :description";
+
+			// Preparar la declaración PDO
+			$statement = $this->db->prepare($query);
+
+			// Bind de los parámetros con el operador "%" para buscar descripciones que contengan la palabra especificada
+			$description = '%' . $description . '%';
+			$statement->bindParam(":description", $description, PDO::PARAM_STR);
+
+			// Ejecutar la consulta
+			$statement->execute();
+
+			// Obtener las transacciones como un arreglo asociativo
+			$transactions = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+			// Verificar si se encontraron transacciones
+			if (empty($transactions)) {
+				// Si no se encontraron transacciones, puedes devolver un mensaje personalizado o un arreglo vacío
+				$response = [
+					"message" => "No se encontraron transacciones con la descripción especificada."
+				];
+			} else {
+				// Si se encontraron transacciones, puedes devolverlas en formato JSON
+				$response = $transactions;
+			}
+
+			return json_encode($response, JSON_PRETTY_PRINT);
+		} catch (PDOException $e) {
+			// En caso de error en la consulta, puedes manejarlo aquí
+			return json_encode(["error" => $e->getMessage()], JSON_PRETTY_PRINT);
+		}
+	}
 
     // Función para buscar transacciones por monto
-    public function getTransactionsByAmount($amount)
-    {
-        // Implementa la lógica para buscar transacciones por monto
-		echo "Imprimir registros por monto";
-    }
+	public function getTransactionsByAmount($amount)
+	{
+		try {
+			// Preparar la consulta SQL para buscar transacciones por monto
+			$query = "SELECT * FROM transactions WHERE amount = :amount";
+
+			// Preparar la declaración PDO
+			$statement = $this->db->prepare($query);
+
+			// Bind del parámetro :amount
+			$statement->bindParam(":amount", $amount, PDO::PARAM_STR);
+
+			// Ejecutar la consulta
+			$statement->execute();
+
+			// Obtener las transacciones como un arreglo asociativo
+			$transactions = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+			// Verificar si se encontraron transacciones
+			if (empty($transactions)) {
+				// Si no se encontraron transacciones, puedes devolver un mensaje personalizado o un arreglo vacío
+				$response = [
+					"message" => "No se encontraron transacciones con el monto especificado."
+				];
+			} else {
+				// Si se encontraron transacciones, puedes devolverlas en formato JSON
+				$response = $transactions;
+			}
+
+			return json_encode($response, JSON_PRETTY_PRINT);
+		} catch (PDOException $e) {
+			// En caso de error en la consulta, puedes manejarlo aquí
+			return json_encode(["error" => $e->getMessage()], JSON_PRETTY_PRINT);
+		}
+	}
 
     // Función para buscar transacciones por tipo
-    public function getTransactionsByType($type)
-    {
-        // Implementa la lógica para buscar transacciones por tipo
-		echo "Imprimir registros por tipo";
-    }
+	public function getTransactionsByType($type)
+	{
+		try {
+			// Preparar la consulta SQL para buscar transacciones por tipo
+			$query = "SELECT * FROM transactions WHERE type = :type";
+
+			// Preparar la declaración PDO
+			$statement = $this->db->prepare($query);
+
+			// Bind del parámetro :type
+			$statement->bindParam(":type", $type, PDO::PARAM_STR);
+
+			// Ejecutar la consulta
+			$statement->execute();
+
+			// Obtener las transacciones como un arreglo asociativo
+			$transactions = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+			// Verificar si se encontraron transacciones
+			if (empty($transactions)) {
+				// Si no se encontraron transacciones, puedes devolver un mensaje personalizado o un arreglo vacío
+				$response = [
+					"message" => "No se encontraron transacciones con el tipo especificado."
+				];
+			} else {
+				// Si se encontraron transacciones, puedes devolverlas en formato JSON
+				$response = $transactions;
+			}
+
+			return json_encode($response, JSON_PRETTY_PRINT);
+		} catch (PDOException $e) {
+			// En caso de error en la consulta, puedes manejarlo aquí
+			return json_encode(["error" => $e->getMessage()], JSON_PRETTY_PRINT);
+		}
+	}
 
     // Función para buscar transacciones por rango de fechas
-    public function getTransactionsByDateRange($dateInicio, $dateFinal)
-    {
-        // Implementa la lógica para buscar transacciones por rango de fechas
-		echo "Imprimir registros por fechas";
-    }
+	public function getTransactionsByDateRange($dateInicio, $dateFinal)
+	{
+		try {
+			// Preparar la consulta SQL para buscar transacciones por rango de fechas
+			$query = "SELECT * FROM transactions WHERE date >= :dateInicio AND date <= :dateFinal";
 
+			// Preparar la declaración PDO
+			$statement = $this->db->prepare($query);
+
+			// Bind de los parámetros :dateInicio y :dateFinal
+			$statement->bindParam(":dateInicio", $dateInicio, PDO::PARAM_STR);
+			$statement->bindParam(":dateFinal", $dateFinal, PDO::PARAM_STR);
+
+			// Ejecutar la consulta
+			$statement->execute();
+
+			// Obtener las transacciones como un arreglo asociativo
+			$transactions = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+			// Verificar si se encontraron transacciones
+			if (empty($transactions)) {
+				// Si no se encontraron transacciones, puedes devolver un mensaje personalizado o un arreglo vacío
+				$response = [
+					"message" => "No se encontraron transacciones en el rango de fechas especificado."
+				];
+			} else {
+				// Si se encontraron transacciones, puedes devolverlas en formato JSON
+				$response = $transactions;
+			}
+
+			return json_encode($response, JSON_PRETTY_PRINT);
+		} catch (PDOException $e) {
+			// En caso de error en la consulta, puedes manejarlo aquí
+			return json_encode(["error" => $e->getMessage()], JSON_PRETTY_PRINT);
+		}
+	}
+
+	// Función para agregar una transacción nueva
 	public function addTransaction($data)
 	{
-		// Mostrar los valores recibidos
-		$descripcion = $data['descripcion'];
-		$monto = $data['monto'];
-		$tipo = $data['tipo'];
-		$date = $data['date'];
-		
-		// Construir un arreglo con los valores
-		$result = [
-			"descripcion" => $descripcion,
-			"monto" => $monto,
-			"tipo" => $tipo,
-			"date" => $date
-		];
+		try {
+			// Obtener los datos del formulario
+			$descripcion = $data['descripcion'];
+			$monto = $data['monto'];
+			$tipo = $data['tipo'];
+			$date = $data['date'];
 
-		// Construir la respuesta JSON
-		echo json_encode($result, JSON_PRETTY_PRINT); // Muestra la respuesta JSON
+			// Preparar la consulta SQL INSERT
+			$query = "INSERT INTO transactions (description, amount, type, date) VALUES (:descripcion, :monto, :tipo, :date)";
+			$statement = $this->db->prepare($query);
+
+			// Enlazar los parámetros
+			$statement->bindParam(":descripcion", $descripcion, PDO::PARAM_STR);
+			$statement->bindParam(":monto", $monto, PDO::PARAM_STR);
+			$statement->bindParam(":tipo", $tipo, PDO::PARAM_STR);
+			$statement->bindParam(":date", $date, PDO::PARAM_STR);
+
+			// Ejecutar la consulta SQL INSERT
+			$statement->execute();
+
+			// Verificar si se insertó correctamente
+			if ($statement->rowCount() > 0) {
+				// Si se insertó correctamente, construir una respuesta JSON
+				$response = [
+					"message" => "Registro agregado exitosamente"
+				];
+			} else {
+				// Si no se insertó correctamente, mostrar un mensaje de error
+				$response = [
+					"message" => "No se pudo agregar el registro"
+				];
+			}
+
+			return json_encode($response, JSON_PRETTY_PRINT);
+		} catch (PDOException $e) {
+			// En caso de error en la consulta, puedes manejarlo aquí
+			return json_encode(["error" => $e->getMessage()], JSON_PRETTY_PRINT);
+		}
 	}
 
 	// Función para actualizar una transacción existente
 	public function updateTransaction($transactionId, $data)
 	{
-		// Ejemplo de actualización de datos
-		$descripcion = $data['descripcion'];
-		$monto = $data['monto'];
-		$tipo = $data['tipo'];
-		$date = $data['date'];
+		try {
+			// Obtener los datos del formulario
+			$descripcion = $data['descripcion'];
+			$monto = $data['monto'];
+			$tipo = $data['tipo'];
+			$date = $data['date'];
 
-		// Construir la respuesta JSON
-		$response = [
-			"id" => $transactionId, // Usar $transactionId en lugar de $id
-			"descripcion" => $descripcion,
-			"monto" => $monto,
-			"tipo" => $tipo,
-			"date" => $date
-		];
+			// Preparar la consulta SQL UPDATE
+			$query = "UPDATE transactions SET description = :descripcion, amount = :monto, type = :tipo, date = :date WHERE id = :transactionId";
+			$statement = $this->db->prepare($query);
 
-		return json_encode($response, JSON_PRETTY_PRINT); // Devuelve la respuesta JSON
+			// Enlazar los parámetros
+			$statement->bindParam(":descripcion", $descripcion, PDO::PARAM_STR);
+			$statement->bindParam(":monto", $monto, PDO::PARAM_STR);
+			$statement->bindParam(":tipo", $tipo, PDO::PARAM_STR);
+			$statement->bindParam(":date", $date, PDO::PARAM_STR);
+			$statement->bindParam(":transactionId", $transactionId, PDO::PARAM_INT);
+
+			// Ejecutar la consulta SQL UPDATE
+			$statement->execute();
+
+			// Verificar si se actualizó correctamente
+			if ($statement->rowCount() > 0) {
+				// Si se actualizó correctamente, construir una respuesta JSON
+				$response = [
+					"message" => "Registro actualizado exitosamente"
+				];
+			} else {
+				// Si no se actualizó correctamente, mostrar un mensaje de error
+				$response = [
+					"message" => "No se pudo actualizar el registro o el registro no existe"
+				];
+			}
+
+			return json_encode($response, JSON_PRETTY_PRINT);
+		} catch (PDOException $e) {
+			// En caso de error en la consulta, puedes manejarlo aquí
+			return json_encode(["error" => $e->getMessage()], JSON_PRETTY_PRINT);
+		}
 	}
 
     // Función para eliminar una transacción por su ID
 	public function deleteTransaction($transactionId)
 	{
-		// Construir la respuesta JSON
-		$response = [
-			"id" => $transactionId
-		];
+		try {
+			// Preparar la consulta SQL DELETE
+			$query = "DELETE FROM transactions WHERE id = :transactionId";
+			$statement = $this->db->prepare($query);
 
-		return json_encode($response, JSON_PRETTY_PRINT); // Devuelve la respuesta JSON
+			// Enlazar el parámetro :transactionId
+			$statement->bindParam(":transactionId", $transactionId, PDO::PARAM_INT);
+
+			// Ejecutar la consulta SQL DELETE
+			$statement->execute();
+
+			// Verificar si se eliminó correctamente
+			if ($statement->rowCount() > 0) {
+				// Si se eliminó correctamente, construir una respuesta JSON
+				$response = [
+					"message" => "Registro eliminado exitosamente",
+					"id" => $transactionId
+				];
+			} else {
+				// Si no se eliminó correctamente o el registro no existe, mostrar un mensaje de error
+				$response = [
+					"message" => "No se pudo eliminar el registro o el registro no existe",
+					"id" => $transactionId
+				];
+			}
+
+			return json_encode($response, JSON_PRETTY_PRINT);
+		} catch (PDOException $e) {
+			// En caso de error en la consulta, puedes manejarlo aquí
+			return json_encode(["error" => $e->getMessage()], JSON_PRETTY_PRINT);
+		}
 	}
 
 }
